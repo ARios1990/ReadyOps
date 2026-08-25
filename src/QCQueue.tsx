05a16912681136dad238de60f6178fa894159e68
Masters Ready Services <mastersreadyservices2025@gmail.com>
2026-08-24 19:34:21 -0600

    Updated QCQueue.tsx

--- src/QCQueue.tsx
+++ src/QCQueue.tsx
@@ -1508,8 +1508,6 @@ function ReviewDialog(props: DialogProps) {
               />
             </div>
             <div className="rounded-xl border border-amber-200 bg-amber-50 p-4">
-            />
-            <div className="rounded-xl border border-amber-200 bg-amber-50 p-4">
               
               <p className="mt-2 whitespace-pre-line text-sm text-amber-900">
                 {props.row.portal?.requirements_short ||
