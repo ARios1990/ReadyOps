9d4a0cccc78e94f1bf23b9a96229bd0149c1216c
Masters Ready Services <mastersreadyservices2025@gmail.com>
2026-08-24 19:30:08 -0600

    Updated QCQueue.tsx

--- src/QCQueue.tsx
+++ src/QCQueue.tsx
@@ -1499,7 +1499,8 @@ function ReviewDialog(props: DialogProps) {
               onChange={(value) => props.change("recording_url", value)}
               onShareChange={(value) =>
                 props.change("share_recording_with_company", value)
-                                      />
+              }
+            />
             <div className="rounded-xl border border-slate-200 bg-slate-50 p-4">
               <ClientLeadTemplate
                 lead={{ ...props.row.lead, ...props.values }}
