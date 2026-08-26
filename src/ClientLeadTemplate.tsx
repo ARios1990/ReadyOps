27dc2dee6e03003caa3ef52c32bfa4c2fc5a513a
Masters Ready Services <mastersreadyservices2025@gmail.com>
2026-08-25 19:46:13 -0600

    Updated ClientLeadTemplate.tsx

--- src/ClientLeadTemplate.tsx
+++ src/ClientLeadTemplate.tsx
@@ -219,7 +219,7 @@ export function ClientLeadTemplate({
               <button
                 key={label}
                 type="button"
-                className={'min-w-0 flex-1 whitespace-nowrap rounded-lg px-3 py-2 text-[11px] font-black text-white shadow-sm ' + tone}
+                className={'min-w-0 flex-1 whitespace-nowrap rounded-lg px-2.5 py-2 text-[10px] font-black text-white shadow-sm ' + tone}
               >
                 {label}
               </button>
